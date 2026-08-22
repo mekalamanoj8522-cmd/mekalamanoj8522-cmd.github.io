297f25afc753b879abae34645b2b36438355e376
mekalamanoj8522-cmd <mekalamanoj8522@gmail.com>
2026-08-22 22:57:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function goHome() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+function openSearch() {
+    alert("🔍 Search feature coming next!");
+}
+
+function openVideos() {
+    window.location.href = "videos.html";
+}
+
+ function openMusic() {
+    window.location.href = "music.html";
+ }
+
+function openFavorites() {
+    alert("❤️ Favorites coming next!");
+}
+
+function openPlaylists() {
+    alert("🎶 Playlists coming next!");
+}
+
+function openSettings() {
+    alert("⚙️ Settings coming next!");
+}
